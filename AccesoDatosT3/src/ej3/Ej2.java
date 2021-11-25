8fe32726f504700d233fa202d03523bada2e8e43
Julen <julenramon792@gmail.com>
2021-11-25 13:57:05 +0100

    CSV Writer

--- AccesoDatosT3/src/ej3/Ej2.java
+++ AccesoDatosT3/src/ej3/Ej2.java
@@ -1,22 +1,27 @@
 package ej3;
 
 import java.io.FileReader;
+import java.io.IOException;
+
 import com.opencsv.CSVReader;
 
 public class Ej2 {
 	public static void main(String[] args) {
 		try {
-			String archCSV = "src/ej3/Restaurants.csv";
-			CSVReader csvReader = new CSVReader(new FileReader(archCSV));
+			String csv = "src/ej3/Restaurants.csv";
+			CSVReader reader = new CSVReader(new FileReader(csv));
 			String[] fila = null;
-			while((fila = csvReader.readNext()) != null) {
-			    System.out.println(fila[0]
-			              + " | " + fila[1]
-			              + " |  " + fila[2]);
+			while((fila = reader.readNext()) != null) {
+			    if (fila[4].matches("^[6][0-9]*$")) {
+				System.out.println(fila[0]
+			    		 + " | " + fila[1]
+			    		 + " | " + fila[2]
+			             + " | " + fila[3]
+			             + " | " + fila[4]);
+			    }
 			}
-
-			csvReader.close();
-		} catch (Exception e) {
+			reader.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
